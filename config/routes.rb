647c6457a260aae350f2916b01dff3a32a6d593c
Oleg Grigoriev <mehanoid.ru@gmail.com>
2013-09-21 23:12:17 +0400

    Add devise user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RubyHotel::Application.routes.draw do
+  devise_for :users, path_prefix: :admin
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +40,7 @@ RubyHotel::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
