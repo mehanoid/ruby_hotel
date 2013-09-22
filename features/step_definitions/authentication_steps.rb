639d3e62137b2cb2e5a2aa71e280658d001ec00e
Oleg Grigoriev <mehanoid.ru@gmail.com>
2013-09-23 03:50:15 +0400

    Main page for admin

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -5,3 +5,12 @@ end
 Допустим(/^я зарегистрирован в системе$/) do
   @user = create(:user)
 end
+
+Если(/^я захожу на страницу входа в систему$/) do
+  visit admin_path
+  click_on 'Login'
+end
+
+Если(/^я ввожу верные данные для входа$/) do
+  pending
+end
