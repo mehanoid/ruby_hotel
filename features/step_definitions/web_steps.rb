4f3342c6ac9c95f2e8b0315f5d614464811d2d0c
Oleg Grigoriev <mehanoid.ru@gmail.com>
2013-09-24 17:02:53 +0400

    Add rooms and rooms categories features

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -4,8 +4,20 @@
   end
 end
 
+Если(/ввожу "([^"]*)" в поле "([^"]*)"$/) do |value, field|
+  fill_in field, with: value
+end
+
+Если(/ввожу в поле "([^"]*)" следующий текст:$/) do |field, text|
+  fill_in field, with: text
+end
+
 Если(/нажимаю кнопку "([^"]*)"$/) do |button|
   click_button button
+end
+
+Если(/нажимаю "([^"]*)"$/) do |clickable|
+  click_on clickable
 end
 
 То(/должен увидеть "([^"]*)"$/) do |text|
