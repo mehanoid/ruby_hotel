93223f0233ef8071a4e73af445f24d2543af1544
Oleg Grigoriev <mehanoid.ru@gmail.com>
2013-10-02 19:14:01 +0400

    Add datepicker for reservation date

--- features/step_definitions/reservation_steps.rb
+++ features/step_definitions/reservation_steps.rb
@@ -4,6 +4,11 @@ end
 
 Если(/выбираю для бронирования категорию "([^"]*)"$/) do |category|
   within 'div.category', text: Regexp.new(category) do
-
+    click_on 'Забронировать'
   end
+end
+
+Если(/выбираю дату заезда и дату выезда$/) do
+  fill_in 'Дата заезда', with: '2013-10-01'
+  fill_in 'Дата выезда', with: '2013-10-05'
 end
