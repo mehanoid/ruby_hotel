a856491ac171ec05a9afa6560821527abc588973
Oleg Grigoriev <mehanoid.ru@gmail.com>
2013-10-12 02:19:51 +0400

    Update gemfile and use databasecleaner truncation strategy for javascript

--- features/support/env.rb
+++ features/support/env.rb
@@ -54,7 +54,7 @@ end
 # Possible values are :truncation and :transaction
 # The :transaction strategy is faster, but might give you threading problems.
 # See https://github.com/cucumber/cucumber-rails/blob/master/features/choose_javascript_database_strategy.feature
-Cucumber::Rails::Database.javascript_strategy = :deletion
+Cucumber::Rails::Database.javascript_strategy = :truncation
 Capybara.javascript_driver = :webkit
 Capybara.register_driver :selenium do |app|
   Capybara::Selenium::Driver.new(app, :browser => :chrome)
