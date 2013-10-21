34d05ebdbf2939e475cd7c70f9c1c645605951d7
Oleg Grigoriev <mehanoid.ru@gmail.com>
2013-10-21 22:43:03 +0400

    RoomCategory#free_room search room that not reserved and not occupied

--- app/models/room_category.rb
+++ app/models/room_category.rb
@@ -4,7 +4,11 @@ class RoomCategory < ActiveRecord::Base
   has_many :rooms, foreign_key: :category_id, inverse_of: :category, dependent: :destroy
 
   def free_room(arrival, departure)
-    occupied_rooms = rooms.joins(:reservations).merge(Reservation.overlapping_with(arrival, departure))
-    rooms.where { id.not_in occupied_rooms.select(:id) }.first
+    reserved_rooms = rooms.joins(:reservations).merge(Reservation.overlapping_with(arrival, departure))
+    occupied_rooms = rooms.joins(:placements).merge(Placement.overlapping_with(arrival, departure))
+    rooms.where {
+      (id.not_in reserved_rooms.select(:id)) &
+      (id.not_in occupied_rooms.select(:id))
+    }.first
   end
 end
